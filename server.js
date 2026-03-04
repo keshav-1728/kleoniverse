b0e6c52e310a30893f003a7f4a5ab06ea0df8e2e
keshav-1728 <keshavwork08@gmail.com>
2026-03-05 01:17:05 +0530

    Fixed server config

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => console.log("Server running")); //added by me by chatgpt
+
 // Import Models
 const User = require('./src/models/User');
 const Product = require('./src/models/Product');
